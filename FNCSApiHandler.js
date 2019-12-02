23e42d436ee92da3dea9bd9f3b1cb267826adbec
bettini lou <lbettini@localhost.localdomain>
2019-12-02 10:46:47 +0100

    query + url in function

--- FNCSApiHandler.js
+++ FNCSApiHandler.js
@@ -1,11 +1,11 @@
-async function launchSncfRequest() {
-    const RequestApi = await fetch('https://ressources.data.sncf.com/api/records/1.0/search/?dataset=referentiel-gares-voyageurs&q=lill').catch();
+async function launchSncfRequest(url,city) {
+    const RequestApi = await fetch(url+city).catch();
     const cityResponse = await RequestApi.json().catch();
     return cityResponse.records.map(function (city) {
         return city.fields.gare_ut_libelle
     });
 }
 
-launchSncfRequest().then(function (cities) {
+launchSncfRequest('https://ressources.data.sncf.com/api/records/1.0/search/?dataset=referentiel-gares-voyageurs&q=','lill').then(function (cities) {
     document.write(cities);
 });
