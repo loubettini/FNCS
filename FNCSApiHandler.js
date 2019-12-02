a14a2aef1249f94f8476090330af3945523880c7
loubettini <loubettini@gmail.com>
2019-12-02 10:56:35 +0100

    wip

--- FNCSApiHandler.js
+++ FNCSApiHandler.js
@@ -1,6 +1,6 @@
 async function launchSncfRequest(url,city) {
-    const RequestApi = await fetch(url+city).catch((e)=>console.log(e));
-    const cityResponse = await RequestApi.json().catch((e)=>console.log(e));
+    const RequestApi = await fetch(url+city).catch(e=>console.log(e));
+    const cityResponse = await RequestApi.json().catch(e=>console.log(e));
     return cityResponse.records.map(function (city) {
         return city.fields.gare_ut_libelle
     });
