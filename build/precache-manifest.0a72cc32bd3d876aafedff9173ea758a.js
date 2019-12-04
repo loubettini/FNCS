71a5ee8c3e22d3a8b7bc7028aa57ae469a283519
loubettini <loubettini@gmail.com>
2019-12-04 15:04:53 +0100

    build

---
+++ build/precache-manifest.0a72cc32bd3d876aafedff9173ea758a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8744338b0fd0a78737b1ae2e07b12fb",
+    "url": "/FNCS/build/index.html"
+  },
+  {
+    "revision": "39975ab4cef4a7abff53",
+    "url": "/FNCS/build/static/js/2.6a22a576.chunk.js"
+  },
+  {
+    "revision": "7c87f9171833175c25b6",
+    "url": "/FNCS/build/static/js/main.1163e46c.chunk.js"
+  },
+  {
+    "revision": "371ebe579c22535579cc",
+    "url": "/FNCS/build/static/js/runtime-main.ee769a73.js"
+  }
+]);
